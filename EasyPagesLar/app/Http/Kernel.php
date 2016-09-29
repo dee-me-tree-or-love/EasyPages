e15654e0e40d7ba1e09a3b3a9efa339157ee8fd0
Dmitry <dmitry-orlov@hotmail.com>
2016-09-29 20:42:43 +0200

    very bad feel

--- EasyPagesLar/app/Http/Kernel.php
+++ EasyPagesLar/app/Http/Kernel.php
@@ -53,5 +53,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'checkUser' => \App\Http\Middleware\checkUser::class,
+		'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
