ccba213e5ddd44d9bb8ed606565bca77791eb564
Dmitry <dmitry-orlov@hotmail.com>
2016-10-06 20:00:47 +0200

    kek

--- EasyPagesIo/www/app/components/account/accctrl.js
+++ EasyPagesIo/www/app/components/account/accctrl.js
@@ -16,17 +16,17 @@ app.controller('AccCtrl', function ($scope, $auth, $http, $rootScope, $ionicHist
 
         // for the ease of use    
         $scope.user = $rootScope.user;
-        if ($scope.user.type !== 'c') {
+        // if ($scope.user.type !== 'c') {
             $scope.moduleTitle = "Your Profile"
             $scope.getUserProfile(response.data.user.id);
             $scope.mdl = 'app/components/account/profilestuff/profilepane.html';
-        }
-        else {
-            $scope.moduleTitle = "Your Company"
-            $scope.usertype = "service provider";
-            $scope.mdl = 'app/components/account/companystuff/companypane.html';
-            $scope.getUserCompany(response.data.user.id);
-        }
+        // }
+        // else {
+        //     $scope.moduleTitle = "Your Company"
+        //     $scope.usertype = "service provider";
+        //     $scope.mdl = 'app/components/account/companystuff/companypane.html';
+        //     $scope.getUserCompany(response.data.user.id);
+        // }
 
     }, function errorCallback(response) {
         $scope.stat = response.data.message;
@@ -37,7 +37,7 @@ app.controller('AccCtrl', function ($scope, $auth, $http, $rootScope, $ionicHist
 //methods - maybe we should add factories for the profiles and companies instead of declaring everythong int he controller? 
 
     $scope.getUserProfile = function getUser(userid) {
-        $url = 'http://localhost:8000/api/eplar/profile/' + userid;
+        $url = 'http://localhost:8000/api/eplar/profile/' + 10;
         $http({
             method: 'GET',
             url: $url
