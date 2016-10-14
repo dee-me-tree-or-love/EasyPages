0867730c1997f51f0274c3656640690d604b0076
Bergshoeff,Daniel D.A <d.bergshoeff@student.fontys.nl>
2016-10-14 10:17:48 +0200

    merge

--- EasyPagesIo/www/app/components/dashboard/dashctrl.js
+++ EasyPagesIo/www/app/components/dashboard/dashctrl.js
@@ -32,6 +32,9 @@ appcntrls.controller('DashCtrl', function ($scope, $http, $rootScope) {
         $scope.myOrderBy = x;
     }
 
+    $scope.priceRange = function(item) {
+        return (parseInt(item['min-acceptable-price']) >= $scope.lower_price_bound && parseInt(item['max-acceptable-price']) <= $scope.upper_price_bound);
+    };
 
 });
 
